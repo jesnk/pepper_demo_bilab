6bb4dea8415386aabf4353a35d2c3a62e3e8c8c8
jesn1219 <jesn1219@gmail.com>
2020-11-30 17:43:36 +0900

    yjLee's file updated by jesnk

--- main.py
+++ main.py
@@ -3,64 +3,51 @@
 import qi
 import argparse
 import sys
-import math
+import time
 
 
-def main(session):
+
+
+def main(app):
+    session = app.session
     tts = session.service("ALTextToSpeech")
-
     # tts.setLanguage("Korean")
     tts.setLanguage("English")
-    # Say Emile in english
-    tts.say("hello")
+    # tts.say("hello! how may I help you?")
+    tabletService = session.service("ALTabletService")
+
+    # tabletService.showWebview()
+
+    signalID = 0
+
+    def callback(x, y):
+        print "coordinate are x: ", x, " y: ", y
+        if x > 640:
+            # disconnect the signal
+            tabletService.onTouchDown.disconnect(signalID)
+            app.stop()
 
 
-
-############## TEST CODE ################
-def main2(session) :
-    posture_service = session.service("ALRobotPosture")
-    posture_service.goToPosture("Crouch",2.0)
-
-def main3(session) :
-    navigation_service = session.service("ALNavigation")
-    motion_service = session.service("ALMotion")
-    posture_service = session.service("ALRobotPosture")
-    posture_service.goToPosture("StandInit",0.5)
-    if False : #navigation_service.navigateTo(1.0,0.0) :
-        print("True")
-    else :
-        print("False")
-    if motion_service.moveTo(-1.0, 0.0, 0.0): # math.pi) :#math.pi) :
-        print("True")
-    else :
-        print("False")
-    #motion_service.moveTo(0.0,0.0,0.0)
-    #navigation_service.startFreeZoneUpdate()
-
-    '''
-    navigationProxy.moveAlong(["Composed", \
-                               ["Holonomic", ["Line", [1.0, 0.0]], 0.0, 5.0], \
-                               ["Holonomic", ["Line", [-1.0, 0.0]], 0.0, 10.0]])
-    '''
-##################################
-
-
+    signalID = tabletService.onTouchDown.connect(callback)
+    app.run()
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", type=str, default="192.168.1.123",
-                        help="Robot IP address. On robot or Local Naoqi: use '192.168.1.123'.")
+    parser.add_argument("--ip", type=str, default="192.168.1.125",
+                        help="Robot IP address. On robot or Local Naoqi: use '192.168.1.125'.")
     parser.add_argument("--port", type=int, default=9559,
                         help="Naoqi port number")
 
     args = parser.parse_args()
     session = qi.Session()
     try:
-        session.connect("tcp://" + args.ip + ":" + str(args.port))
+        connection_url = "tcp://" + args.ip + ":" + str(args.port)
+        app = qi.Application(["TabletModule", "--qi-url=" + connection_url])
+        app.start()
     except RuntimeError:
         print ("Can't connect to Naoqi at ip \"" + args.ip + "\" on port " + str(args.port) +".\n"
                "Please check your script arguments. Run with -h option for help.")
         sys.exit(1)
-    main(session)
+    main(app)
     print("Finishied")
